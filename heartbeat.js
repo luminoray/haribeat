0b2eeb5a0d7ac6165735e62e51e23cd6e01f404c
luminoray <zoestell37@gmail.com>
2021-05-27 00:52:56 -0700

    Initial commit

---
+++ heartbeat.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const playAudio = function (lastId) {
+        let rng = Math.ceil(Math.random() * 53);
+        // Prevent the last sample from being selected twice in a row.
+        if (rng >= lastId) {
+            rng++;
+        }
+        let name = 'heartbeat-' + ((rng <= 9) ? '0' : '') + rng;
+        document.getElementById(name).play();
+        setTimeout(function () {
+            playAudio(rng)
+        }, 800);
+    }
+
+    // Iterate and append all audio files.
+    let container = document.getElementById('audio-container');
+    let i, audio, source, name;
+    for (i = 1; i <= 54; i++) {
+        name = 'heartbeat-' + ((i <= 9) ? '0' : '') + i;
+        audio = document.createElement('audio');
+        audio.setAttribute('preload', 'auto');
+        audio.id = name;
+        source = document.createElement('source');
+        source.setAttribute('src', './beats/' + name + '.wav');
+        source.setAttribute('type', 'audio/wav');
+        audio.append(source);
+        container.append(audio);
+    }
+
+    document.getElementById('play-button').onclick = function () {
+        if (! this.hasAttribute('disabled')) {
+            playAudio();
+            this.setAttribute('disabled', '');
+        }
+    }
+});
