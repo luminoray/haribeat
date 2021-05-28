95908c17806aeaf9031aabdc4159889d02b6087b
luminoray <zoestell37@gmail.com>
2021-05-27 22:26:01 -0700

    Changed samples to ogg. Stabilized sample beats.

--- heartbeat.js
+++ heartbeat.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const playAudio = function (lastId) {
-        let rng = Math.ceil(Math.random() * 53);
+        let rng = Math.ceil(Math.random() * 52);
         // Prevent the last sample from being selected twice in a row.
         if (rng >= lastId) {
             rng++;
@@ -15,14 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // Iterate and append all audio files.
     let container = document.getElementById('audio-container');
     let i, audio, source, name;
-    for (i = 1; i <= 54; i++) {
+    for (i = 1; i <= 53; i++) {
         name = 'heartbeat-' + ((i <= 9) ? '0' : '') + i;
         audio = document.createElement('audio');
         audio.setAttribute('preload', 'auto');
         audio.id = name;
         source = document.createElement('source');
-        source.setAttribute('src', './beats/' + name + '.wav');
-        source.setAttribute('type', 'audio/wav');
+        source.setAttribute('src', './beats/ogg/' + name + '.ogg');
+        source.setAttribute('type', 'audio/ogg');
         audio.append(source);
         container.append(audio);
     }
